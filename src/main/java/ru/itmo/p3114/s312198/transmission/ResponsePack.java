0fd9e2355330da1b76ad5f085f3e76bd30459aa4
Yars2021 <yaroslav.sukhovey@yandex.ru>
2021-06-11 19:54:51 +0300

    command-utils library v2.4

--- src/main/java/ru/itmo/p3114/s312198/transmission/ResponsePack.java
+++ src/main/java/ru/itmo/p3114/s312198/transmission/ResponsePack.java
@@ -5,12 +5,26 @@ import java.util.ArrayList;
 
 public class ResponsePack implements Serializable {
     private final ArrayList<String> output;
+    private Boolean allowed;
 
     public ResponsePack(ArrayList<String> output) {
         this.output = output;
     }
 
+    public ResponsePack(ArrayList<String> output, Boolean allowed) {
+        this.output = output;
+        this.allowed = allowed;
+    }
+
+    public void setAllowed(Boolean allowed) {
+        this.allowed = allowed;
+    }
+
     public ArrayList<String> getOutput() {
         return output;
+    }
+
+    public Boolean getAllowed() {
+        return allowed;
     }
 }
